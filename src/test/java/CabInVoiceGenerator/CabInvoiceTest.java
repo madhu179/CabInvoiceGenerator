d956ba0e2476e1fca7a6ce43230aa8c697f37c5b
madhu179 <madhubabu232343@gmail.com>
2020-10-20 18:21:02 +0530

    Refactoring test cases for using the enum the right way and for dependency injection

--- src/test/java/CabInVoiceGenerator/CabInvoiceTest.java
+++ src/test/java/CabInVoiceGenerator/CabInvoiceTest.java
@@ -5,20 +5,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import CabInVoiceGenerator.CabInvoiceMain.Subscription_Type;
-
 public class CabInvoiceTest {
 
 	CabInvoiceMain cabInvoiceMain;
+	RidesRepository ridesRepository;
 
 	@Before
 	public void createObject() {
-		cabInvoiceMain = new CabInvoiceMain();
-		Ride[] ridesArray= {new Ride(5.0,5),new Ride(0.2,2)};	
+		ridesRepository = new RidesRepository();
+		cabInvoiceMain = new CabInvoiceMain(ridesRepository);
+		Ride[] ridesArray= {new Ride(5.0,5,CabRideEnum.NORMAL),new Ride(0.2,2,CabRideEnum.NORMAL)};	
 		cabInvoiceMain.addRidesToRepo(1,ridesArray);		
-		Ride[] ridesArray21 = {new Ride(1.0,5)};
+		Ride[] ridesArray21 = {new Ride(1.0,5,CabRideEnum.PREMIMUM)};
 		cabInvoiceMain.addRidesToRepo(2,ridesArray21);
-		Ride[] ridesArray22 = {new Ride(0.2,2),new Ride(2.0,5)};
+		Ride[] ridesArray22 = {new Ride(0.2,2,CabRideEnum.PREMIMUM),new Ride(2.0,5,CabRideEnum.PREMIMUM)};
 		cabInvoiceMain.addRidesToRepo(2,ridesArray22);	
 	}
 
@@ -26,7 +26,7 @@ public class CabInvoiceTest {
 	public void givenDistanceAndTimeShouldReturnFare() {
 		double distance = 5;
 		int time = 5;
-		double fare = cabInvoiceMain.calculateFare(distance, time,Subscription_Type.NORMAL);
+		double fare = cabInvoiceMain.calculateFare(distance, time);
 		assertEquals(55.0, fare, 0.0);
 	}
 	
@@ -34,36 +34,36 @@ public class CabInvoiceTest {
 	public void givenLowDistanceAndTimeShouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 2;
-		double fare = cabInvoiceMain.calculateFare(distance, time,Subscription_Type.NORMAL);
+		double fare = cabInvoiceMain.calculateFare(distance, time);
 		assertEquals(5.0, fare, 0.0);
 	}
 	
 	@Test
 	public void givenDistanceAndTimeForMultipleridesShouldReturnFare() {
-		Ride[] rides = {new Ride(5.0,5),new Ride(0.1,2)};
-		double totalFare = cabInvoiceMain.calculateTotalFare(rides,Subscription_Type.NORMAL);
+		Ride[] rides = {new Ride(5.0,5,CabRideEnum.NORMAL),new Ride(0.1,2,CabRideEnum.NORMAL)};
+		double totalFare = cabInvoiceMain.calculateTotalFare(rides);
 		assertEquals(60.0, totalFare, 0.0);
 	}
 	
 	@Test
 	public void givenDistanceAndTimeForMultipleridesShouldReturnSummary() {
-		Ride[] rides = {new Ride(5.0,5),new Ride(0.1,2)};
+		Ride[] rides = {new Ride(5.0,5,CabRideEnum.NORMAL),new Ride(0.1,2,CabRideEnum.NORMAL)};
 		InvoiceSummary ecpectedInvoice = new InvoiceSummary(2,60.0);
-		InvoiceSummary outputInvoice = cabInvoiceMain.generateSummary(rides,Subscription_Type.NORMAL);
+		InvoiceSummary outputInvoice = cabInvoiceMain.generateSummary(rides);
 		assertEquals(ecpectedInvoice, outputInvoice);
 	}
 	
 	@Test
 	public void givenUserIdShouldReturnSummary() {
-		InvoiceSummary ecpectedInvoice = new InvoiceSummary(3,45.0);
-		InvoiceSummary outputInvoice = cabInvoiceMain.generateInvoice(2,Subscription_Type.NORMAL);
+		InvoiceSummary ecpectedInvoice = new InvoiceSummary(2,60.0);
+		InvoiceSummary outputInvoice = cabInvoiceMain.generateInvoice(1);
 		assertEquals(ecpectedInvoice, outputInvoice);
 	}
 	
 	@Test
 	public void givenPremimumTypeAndUserIdShouldReturnSummary() {
-		InvoiceSummary ecpectedInvoice = new InvoiceSummary(2,105.0);
-		InvoiceSummary outputInvoice = cabInvoiceMain.generateInvoice(1,Subscription_Type.PREMIMUM);
+		InvoiceSummary ecpectedInvoice = new InvoiceSummary(3,85.0);
+		InvoiceSummary outputInvoice = cabInvoiceMain.generateInvoice(2);
 		assertEquals(ecpectedInvoice, outputInvoice);
 	}
 
